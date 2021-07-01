6b940f53920f8c427f3b21f82bd0c62a77271df3
Jesse Kuykendall <84601365+techyjesse@users.noreply.github.com>
2021-06-30 23:06:36 -0700

    Initial Project Commits

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+var Family= "Variables", A="This is string A.", B="And this is a second string. " //This is a family of variables that can be called in a function.
+var Variables=A.fontcolor("green");
+var Variables=A.fontcolor("Red");
+
+
+window.alert("This is a window alert");
+
+function My_First_Function() {
+    var str="This text is green!";
+    var result= str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML=result; //This changes the color of the button text created in the HTML doc once the user clicks it. 
+}
+
+document.write("Lisa told Bart, \"Knock it off! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");//This displays text in the HTML doc.
+
+var Family= "The Kuykendalls", Dad="Jesse", Mom="Denise", Son="Jesse", Sister="Hanna"; //This classes multiple variables in ones statement that can be called individually.
+document.write(Dad);
+
+var blues="I have the blues.";
+var blues=blues.fontcolor("blue");
+document.write(blues);
+
+var price1= 5;
+var price2= 14;
+var total= price1 + price2;
+document.write(price1 + price2);
+
